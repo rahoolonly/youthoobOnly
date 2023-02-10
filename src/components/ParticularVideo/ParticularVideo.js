b00fb97ff9a055ac5e25b1d3f347a3b5e4f435fc
rahoolonly <rahoolonly@gmail.com>
2023-02-10 15:08:47 +0530

    making particular video responsive

--- src/components/ParticularVideo/ParticularVideo.js
+++ src/components/ParticularVideo/ParticularVideo.js
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 const ParticularVideo = () => {
   const { id } = useParams();
   const { data, isLoading, isError } = useGetParticularVideoDetailsQuery(id);
+  console.log(data);
 
   if (isError) {
     return <h3>There is some error</h3>;
@@ -30,20 +31,54 @@ const ParticularVideo = () => {
         </div>
 
         <div className="particular-video-text">
-          <div className=" particular-video-title"></div>
-          {data?.title}
+          <div className=" particular-video-title"> {data?.title}</div>
+         <div className="mobilePointViews">
+         <div>
+                {data.stats.views > 1000000
+                  ? (data.stats.views / 1000000).toFixed(1) + "M views"
+                  : data.stats.views > 100000
+                  ? Math.floor(data.stats.views / 1000) + "k views"
+                  : data.stats.views > 10000
+                  ? (data.stats.views / 1000).toFixed(1) + "k views"
+                  : data.stats.views > 1000
+                  ? (data.stats.views / 1000).toFixed(1) + "k views"
+                  : data.stats.views == null
+                  ? data.stats.viewers + " views"
+                  : data.stats.views + " views"}{" "}
+              </div>
+              {data?.publishedDate}
+         </div>
           <div className="particular-video-logo-with-text">
             <div className="particular-video-logo">
               <img src={data?.author?.avatar[1].url} alt="" />
-            </div>
-            <div className="particular-video-channelName">
-              {data?.author.title}
+              <div className="particularChannelNameWithSubs">
+                <span className="particular-video-channelName">
+                  {" "}
+                  {data?.author.title}
+                </span>
+                <span className="particularVideoSubs">
+                  {data?.author.stats.subscribersText}
+                </span>
+              </div>
             </div>
           </div>
+              <hr />
 
           <div className="desc-with-views">
             <div className="particularVideo-views">
-              <div>{data?.stats.views} views </div>
+              <div>
+                {data.stats.views > 1000000
+                  ? (data.stats.views / 1000000).toFixed(1) + "M views"
+                  : data.stats.views > 100000
+                  ? Math.floor(data.stats.views / 1000) + "k views"
+                  : data.stats.views > 10000
+                  ? (data.stats.views / 1000).toFixed(1) + "k views"
+                  : data.stats.views > 1000
+                  ? (data.stats.views / 1000).toFixed(1) + "k views"
+                  : data.stats.views == null
+                  ? data.stats.viewers + " views"
+                  : data.stats.views + " views"}{" "}
+              </div>
               {data?.publishedDate}
             </div>
 
